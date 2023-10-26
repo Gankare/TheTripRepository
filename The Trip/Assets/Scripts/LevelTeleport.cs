de783ad5ff8ab504dfe18f7615e98945631456a0
Gankare <filipsalomonsson01@gmail.com>
2023-10-26 13:09:13 +0200

    GameScene updated

--- The Trip/Assets/Scripts/LevelTeleport.cs
+++ The Trip/Assets/Scripts/LevelTeleport.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -5,37 +6,47 @@ using UnityEngine;
 
 public class LevelTeleport : MonoBehaviour
 {
+    public GameObject interactText;
     public GameObject mainCamera;
     public Transform area1;
     public Transform area2;
     public Transform area3;
     public Transform area4;
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (mainCamera.GetComponent<CameraFollow>().currentStage == 1)
+            interactText.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.E))
             {
-                collision.transform.position = area1.transform.position;
-            }
+                if (mainCamera.GetComponent<CameraFollow>().currentStage == 1)
+                {
+                    collision.transform.position = area1.transform.position;
+                }
 
-            if (mainCamera.GetComponent<CameraFollow>().currentStage == 2)
-            {
-                collision.transform.position = area2.transform.position;
-            }
+                if (mainCamera.GetComponent<CameraFollow>().currentStage == 2)
+                {
+                    collision.transform.position = area2.transform.position;
+                }
 
-            if (mainCamera.GetComponent<CameraFollow>().currentStage == 3)
-            {
-                collision.transform.position = area3.transform.position;
-            }
+                if (mainCamera.GetComponent<CameraFollow>().currentStage == 3)
+                {
+                    collision.transform.position = area3.transform.position;
+                }
 
-            if (mainCamera.GetComponent<CameraFollow>().currentStage == 4)
-            {
-                collision.transform.position = area4.transform.position;
-            }
+                if (mainCamera.GetComponent<CameraFollow>().currentStage == 4)
+                {
+                    collision.transform.position = area4.transform.position;
+                }
 
-            mainCamera.GetComponent<CameraFollow>().currentStage++;
-        }
+                mainCamera.GetComponent<CameraFollow>().currentStage++;
+            }
+        } 
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            interactText?.SetActive(false);
     }
 }
